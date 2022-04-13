0ac4065d9d719bd1c9158a46765b6555e1c21008
axeli <axeli@Axel>
2022-04-12 22:22:27 -0500

    lp2rrhh

---
+++ LP2SoftRRHHModel/src/pe/edu/pucp/lp2soft/rrhh/model/Empleado.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.lp2soft.rrhh.model;
+
+/**
+ *
+ * @author axeli
+ */
+public class Empleado extends Persona {
+    private String cargo ; 
+    private double sueldo ; 
+    private boolean activo ; 
+    private Area area ;
+    private CuentaUsuario cuentaUsuario ;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public CuentaUsuario getCuentaUsuario() {
+        return cuentaUsuario;
+    }
+
+    public void setCuentaUsuario(CuentaUsuario cuentaUsuario) {
+        this.cuentaUsuario = cuentaUsuario;
+    }
+    
+    
+}
